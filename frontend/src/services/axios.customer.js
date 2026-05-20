67be06e37f018e95747b74b1002ea5ef1144b326
duongmoon <duongtrinh1012@gmail.com>
2026-05-20 08:46:08 +0700

    deploy render.com v2

--- frontend/src/services/axios.customer.js
+++ frontend/src/services/axios.customer.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import nProgress from "nprogress";
 
 
-const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_BACKEND_URL : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_BACKEND_URL : ""
 const instance = axios.create({
     baseURL: BASE_URL
 });
