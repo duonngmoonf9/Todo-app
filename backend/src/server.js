1c5169b945dfcebf98f657f51100f68551e284f1
duongmoon <duongtrinh1012@gmail.com>
2026-05-20 08:31:14 +0700

    deploy render.com

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,34 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import tasksRoutes from './routes/tasksRouters.js';
 
 
-
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 
 app.use(express.json()); // đây là rào cản middleware, tự động parse json gửi lên server
-app.use(cors({ origin: "http://localhost:5173" }));
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use("/api/tasks", tasksRoutes) // Dinh tuyen route
+
+
+//cau hinh duong dan back ben frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- frontend/src/services/axios.customer.js
+++ frontend/src/services/axios.customer.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import nProgress from "nprogress";
 
+
+const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_BACKEND_URL : "/api"
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL
+    baseURL: BASE_URL
 });
 
 instance.interceptors.request.use(function (config) {
