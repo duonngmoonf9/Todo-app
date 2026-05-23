63b7fe23c910e3f6843a218030f52dc204820606
duongmoon <duongtrinh1012@gmail.com>
2026-05-23 16:42:27 +0700

    upload full v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Toaster } from 'sonner';
-import HomePages from './pages/HomePages-vbeta';
+// import HomePages from './pages/HomePages-vbeta';
+import HomePages from './pages/HomePages';
 import NotFound from './pages/NotFound';
 
 function App() {
